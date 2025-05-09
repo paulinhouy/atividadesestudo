cc4c1948d24bc2b183913a16cf4dcb49f15ad931
paulo <paulofilhorocha22@gmail.com>
2025-05-08 23:24:53 -0300

    EXERCICIO

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-let arr  =[1,2,3,4,5,6,1,2]
-console.log(arr.includes(1))
-console.log(arr.lastIndexOf(1))
-console.log(arr.findIndex(function(el){
-    return el > 2
-}))
-console.log(arr.find(function(el){
-    return el > 2
-}))
-console.log(arr.filter(function(el){
-    return el > 2
-}))
+let arr  = new Array(1,2,3,4,5,6)
+let arr2 = arr.push(7,8,9,20)
+console.log(arr) 
+const arr3 = Array.from(arr)
+arr3.push(10,11,12,13,14,15)
+console.log(arr3)
 
