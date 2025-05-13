feb6975021c7d77f038c65a443d7444cb0099104
paulo <paulofilhorocha22@gmail.com>
2025-05-13 15:41:16 -0300

    pro

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-let arr  = new Array(1,2,3,4,5,6)
-let arr2 = arr.push(7,8,9,20)
-console.log(arr) 
-const arr3 = Array.from(arr)
-arr3.push(10,11,12,13,14,15)
-console.log(arr3)
+let array = [1,2,3]
+let novaArray = [...array,5,6,7]
+console.log(novaArray) // [1, 2, 3, 5, 6, 7]
 
