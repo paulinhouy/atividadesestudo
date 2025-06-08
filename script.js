ae20ca5b738c2a707e5ea9f48eb6753e605181bc
paulo <paulofilhorocha22@gmail.com>
2025-06-08 01:09:20 -0300

    a

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let array = [1,2,3]
-let novaArray = [...array,5,6,7]
-console.log(novaArray) // [1, 2, 3, 5, 6, 7]
+const numeros = [1, 2, 3, 4, 5];
+const [a,,c,,e] = numeros;
 
+console.log(a); // 1
+console.log(c);
+console.log(e) // 1
